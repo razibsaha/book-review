2d23c131c38023c889014c910efb782dd25d4a5f
Razib Saha <rajibpay@gmail.com>
2022-04-03 23:12:44 +0600

    Readme file Updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Norwegian Wood</h1>
+      <h4>A novel by Haruki <Murakami></Murakami></h4>
     </div>
   );
 }
